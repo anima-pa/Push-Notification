964b9121212f44276b15a69654b06fd8d5ef869f
anima-pa <anima1111@outlook.com>
2017-09-16 12:36:51 +0530

    initial commit

---
+++ SamplePush/SamplePush.Android/CrossPushNotificationListener.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PushNotification.Plugin;
+using PushNotification.Plugin.Abstractions;
+using System.Diagnostics;
+
+namespace SamplePush.Droid
+{
+   public class CrossPushNotificationListener : IPushNotificationListener
+    {
+        public CrossPushNotificationListener()
+        {
+        }
+        public void OnError(string message, DeviceType deviceType)
+        {
+            
+        }
+
+        public void OnMessage(global::Newtonsoft.Json.Linq.JObject values, DeviceType deviceType)
+        {
+            Debug.WriteLine(string.Format("Push Notification -recieved", values));
+        }
+
+        public void OnRegistered(string token, DeviceType deviceType)
+        {
+            Debug.WriteLine(string.Format("Push Notification - Device Registered - Token : {0}", token));
+        }
+
+        public void OnUnregistered(DeviceType deviceType)
+        {
+            
+        }
+
+        public bool ShouldShowNotification()
+        {
+            return true;
+        }
+    }
+}
---
+++ SamplePush/SamplePush/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PushNotification.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SamplePush
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SamplePush.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            CrossPushNotification.Current.Register();
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
